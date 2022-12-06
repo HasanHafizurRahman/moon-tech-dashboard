2fd11ff20230a240407bf7cc4a25916f72378dcd
Hasan Shanto <hasanshanto922@gmail.com>
2022-12-07 01:41:45 +0600

    product filtering

--- src/pages/Main/Home.js
+++ src/pages/Main/Home.js
@@ -17,6 +17,20 @@ const Home = () => {
 
   const activeClass = "text-white  bg-indigo-500 border-white";
 
+  let content;
+
+  if(products.length){
+    content = products.map((product) => (
+      <ProductCard key={product.model} product={product} />
+    ))
+  }
+
+  if (products.length && stock) {
+    content = products.filter(product => product.status === true).map((product) => (
+      <ProductCard key={product.model} product={product} />
+    ))
+  }
+
   return (
     <div className='max-w-7xl gap-14 mx-auto my-10'>
       <div className='mb-10 flex justify-end gap-5'>
@@ -38,9 +52,7 @@ const Home = () => {
         </button>
       </div>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-14'>
-        {products.map((product) => (
-          <ProductCard key={product.model} product={product} />
-        ))}
+        {content}
       </div>
     </div>
   );
